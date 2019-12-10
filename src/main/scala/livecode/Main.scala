c486807270ddf82f88aa838bca9a4c3165c8e834
Kai <kai@7mind.io>
2019-12-10 21:24:58 +0000

    update to izumi-0.10.0-M7

--- src/main/scala/livecode/Main.scala
+++ src/main/scala/livecode/Main.scala
@@ -7,12 +7,10 @@ import izumi.distage.plugins.load.PluginLoader.PluginConfig
 import izumi.distage.roles.{RoleAppLauncher, RoleAppMain}
 import izumi.fundamentals.platform.cli.model.raw.RawRoleParams
 import livecode.code.LivecodeRole
-import zio.Task
-import zio.interop.catz._
 
 object Main
   extends RoleAppMain.Default(
-    launcher = new RoleAppLauncher.LauncherF[Task] {
+    launcher = new RoleAppLauncher.LauncherBIO[zio.IO] {
       override val pluginSource = PluginSource(
         PluginConfig(
           debug            = false,
